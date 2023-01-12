a05855d10fd168a6b1fe42cf8611ae0533171850
amu <aamouie01@gmail.com>
2023-01-12 18:18:52 +0330

    [BUILD_ERROR-DemoWithConsole] Add a console-only demo project

---
+++ DemoWithConsole/DemoWithConsole.cpp
@@ -0,0 +1,14 @@
+//#include "GameSystems/ResourceManagement/ResourceManagementSample.h"
+//#include "GameSystems/EntityManagement/EntityManagementSample.h"
+#include "GameSystems/Skill/SkillSample.h"
+
+#include <iostream>
+
+int main()
+{
+	//Samples::ResourceManagement::Main();
+	//Samples::EntityManagement::Main();	
+	//Samples::Skills::Main();
+
+	std::cin.get();
+}
