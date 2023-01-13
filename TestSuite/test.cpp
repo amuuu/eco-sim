ff8d171cee922397428597c024359af0b48b5543
amu <aamouie01@gmail.com>
2023-01-14 00:03:47 +0330

    [BUILD_ERROR-TestSuite] Write a simple test to see if dependencies are resolved for test suite (they're not smh)

--- TestSuite/test.cpp
+++ TestSuite/test.cpp
@@ -1,6 +1,31 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include <fstream>
+
+#include "../ExternalTools/nlohmann-json.hpp"
+using json = nlohmann::json;
+
+#include "../GameSystems/EntityManagement/EntityManager.h"
+#include "../GameSystems/EntityManagement/SimpleDumbEntity.h"
+
+
+TEST(JSONDecoding, OpenningAFile) {
+	
+	std::ifstream f{ "TestJson.json" };
+	
+	json data = json::parse(f);
+	
+	EXPECT_EQ(data["dooode"], 1);
+
+	f.close();
+}
+
+TEST(ENTITY, SimpleInstantiation)
+{
+	using namespace EntityManagement;
+
+	EntityManager entityManager{ false };
+
+	entityManager.EnqueueNewEntity(new SimpleDumbEntity{});
+
 }
