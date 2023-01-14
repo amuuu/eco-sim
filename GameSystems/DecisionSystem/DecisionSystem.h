550693ad49276b2fdca527f09527853bcaea694e
amu <aamouie01@gmail.com>
2023-01-14 15:15:28 +0330

    Add a shared pointer for central decision driver (the name must be changed)

--- GameSystems/DecisionSystem/DecisionSystem.h
+++ GameSystems/DecisionSystem/DecisionSystem.h
@@ -7,8 +7,8 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
-#include <atomic>
 #include <utility>
+#include <memory>
 
 namespace DecisionSystem
 {
@@ -20,23 +20,7 @@ namespace DecisionSystem
 #define SMARTNESS "smartness"
 #define FOODY "foody"
 	
-	class StateOfMindContainer
-	{
-	public:
-		void SetValueForStateOfMind(const StateOfMindId id, float value) { stateOfMindValues[id] = value; }
-		void ChangeValueForStateOfMind(const StateOfMindId id, float value) { stateOfMindValues[id] += value; }
-		float GetValueForStateOfMind(const StateOfMindId id) { return stateOfMindValues[id]; }
-		
-		void PrintAllStateOfMinds()
-		{
-			for (auto const& [key, val] : stateOfMindValues)
-				std::cout << key << ':' << val << std::endl;
-		}
 	
-	private:
-		std::map<const StateOfMindId, float> stateOfMindValues{};
-	};
-
 	struct StateOfMindAffector;
 
 	enum ConditionalAffectiveness { IfAmountIsMore, IfAmountIsLess, None };
@@ -66,10 +50,6 @@ namespace DecisionSystem
 		{ }
 
 		StateOfMindElement() = default;
-		//StateOfMindElement(StateOfMindElement&& some) = delete;
-		//StateOfMindElement(StateOfMindElement& some) = delete;
-
-
 	};
 
 	struct StateOfMindAffector
@@ -96,6 +76,9 @@ namespace DecisionSystem
 	public:
 
 		std::map<StateOfMindId, StateOfMindElement> stateOfMindModels{};
+
+		CentralDecisionDriver() = default;
+		~CentralDecisionDriver() = default;
 
 		void Init()
 		{
@@ -146,4 +129,33 @@ namespace DecisionSystem
 		}
 
 	};
+
+	class StateOfMindContainer
+	{
+	public:
+		void SetValueForStateOfMind(const StateOfMindId id, float value) { stateOfMindValues[id] = value; }
+		void ChangeValueForStateOfMind(const StateOfMindId id, float value) { stateOfMindValues[id] += value; }
+		float GetValueForStateOfMind(const StateOfMindId id) { return stateOfMindValues[id]; }
+
+		void PrintAllStateOfMinds()
+		{
+			for (auto const& [key, val] : stateOfMindValues)
+				std::cout << key << ':' << val << std::endl;
+		}
+
+		void SetCentralDecisionDriver(std::shared_ptr<CentralDecisionDriver> driver) { centralDecisionDriver = driver; }
+
+		void UpdateStateOfMinds()
+		{
+			for (auto const& [key, val] : stateOfMindValues)
+			{
+				ChangeValueForStateOfMind(key, centralDecisionDriver.get()->stateOfMindModels[key].autoUpdateAmount);
+			}
+		}
+
+	private:
+		std::map<const StateOfMindId, float> stateOfMindValues{};
+		std::shared_ptr<CentralDecisionDriver> centralDecisionDriver;
+	};
+
 }
--- GameSystems/DecisionSystem/EntityWithStateOfMind.h
+++ GameSystems/DecisionSystem/EntityWithStateOfMind.h
@@ -3,26 +3,36 @@
 #include "../EntityManagement/Entity.h"
 #include "DecisionSystem.h"
 
+#include <memory>
+
 namespace DecisionSystem
 {
 	
 	class EntityWithStateOfMind : public EntityManagement::Entity, public StateOfMindContainer
 	{
-		CentralDecisionDriver cdd{};
+		// TODO: Move this to outside ///////////////
+		std::shared_ptr<CentralDecisionDriver> cdd;
+		//////////////////////////////////////////////
 
 		virtual void Init() override
 		{
-			cdd.Init();
-		
+			// TODO: Move these to outside ///////////////
+			cdd = std::shared_ptr<CentralDecisionDriver>(new CentralDecisionDriver);
+			cdd->Init();
+			SetCentralDecisionDriver(cdd);
+			//////////////////////////////////////////////
+
+
 			SetValueForStateOfMind(HUNGER, 12.f);
 		}
 		
 		virtual void Update(EntityManagement::Tick tick) override
 		{
-			ChangeValueForStateOfMind(HUNGER, cdd.stateOfMindModels[HUNGER].autoUpdateAmount);
-
 			if (tick % 100000 == 0)
+			{
+				UpdateStateOfMinds();
 				PrintAllStateOfMinds();
+			}
 		}
 	};
 }
