dd3b8e6a9318b2f3d34be5b30fd0b1e9677e539f
amu <aamouie01@gmail.com>
2023-02-09 22:11:52 +0330

    Add timer utility, add new timer class

--- GameSystems/GeneralTools/Timer.h
+++ GameSystems/GeneralTools/Timer.h
@@ -1,85 +1,33 @@
 #pragma once
 
+#include "TimerUtility.h"
+
 #include <chrono>
 #include <functional>
 
+using namespace TimerUtility;
+
 namespace General
 {
-	enum class TimeScaleType
-	{
-		Sec, Min, Hour, Day, Week, Month, Year
-	};
-
-	class BaseTimer
+	class Timer
 	{
 	public:
-		
-		BaseTimer(bool autoStart)
-		{
-			if (autoStart)
-				Start();
-		}
 
-		void Start()
-		{
-			Reset();
-			hasStarted = true;
-		}
+		inline float GetElapsed(TimeScaleType scale) { return ConvertToTimeScale(scale, GetElapsedMillis()); }
 
-		bool HasStarted() 
-		{ 
-			return hasStarted; 
-		}
+		Timer(double timescaleToRealLifeSecond, TimestampType startPoint) : timescaleToRealLifeSecond(timescaleToRealLifeSecond), startPoint(startPoint) { }
+		Timer(double timescaleToRealLifeSecond) : timescaleToRealLifeSecond(timescaleToRealLifeSecond), startPoint(NOW) { }
+		Timer() : timescaleToRealLifeSecond(1.), startPoint(NOW){}
 
-		void Reset()
-		{
-			startTimeStamp = std::chrono::high_resolution_clock::now();
-		}
-
-		void Stop()
-		{
-			hasStarted = false;
-		}
-
-		float Elapsed()
-		{
-			if (!hasStarted) return -1.f;
-
-			return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - startTimeStamp).count() * 0.001f * 0.001f * 0.001f;
-		}
-
-		float ElapsedMillis()
-		{
-			if (!hasStarted) return -1.f;
-
-			return Elapsed() * 1000.0f;
-		}
-
-		inline float GetElapsedInScale(TimeScaleType scale)
-		{
-			switch (scale)
-			{
-			case TimeScaleType::Sec:
-				return ElapsedMillis();
-			case TimeScaleType::Min:
-				return ElapsedMillis() / 60.;
-			case TimeScaleType::Hour:
-				return ElapsedMillis() / (60. * 60.);
-			case TimeScaleType::Day:
-				return ElapsedMillis() / (60. * 60. * 24);
-			case TimeScaleType::Week:
-				return ElapsedMillis() / (60. * 60. * 24 * 7);
-			case TimeScaleType::Month:
-				return ElapsedMillis() / (60. * 60. * 24 * 7 * 30);
-			case TimeScaleType::Year:
-				return ElapsedMillis() / (60. * 60. * 24 * 7 * 365);
-			}
-		}
+		void Reset() { startPoint = NOW; }
 
 	private:
-		
-		std::chrono::time_point<std::chrono::high_resolution_clock> startTimeStamp;
-		
-		bool hasStarted{ false };
+
+		float GetElapsedMillis() { return (1. / timescaleToRealLifeSecond) * GetTimestampsDifferenceMillis(NOW, startPoint); }
+
+		double timescaleToRealLifeSecond{ 1. };
+		TimestampType startPoint;
 	};
+
+
 }
---
+++ GameSystems/GeneralTools/TimerUtility.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <chrono>
+
+namespace TimerUtility
+{
+	#define NOW std::chrono::high_resolution_clock::now()
+
+	using TimestampType = std::chrono::time_point<std::chrono::high_resolution_clock>;
+
+	enum class TimeScaleType
+	{
+		Sec, Min, Hour, Day, Week, Month, Year, None
+	};
+
+	static inline float GetTimestampsDifferenceMillis(TimestampType end, TimestampType start)
+	{
+		return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() * 0.001f * 0.001f * 0.001f;
+	}
+
+	static inline float ConvertToTimeScale(TimeScaleType scale, float timeVal)
+	{
+		switch (scale)
+		{
+		case TimeScaleType::Sec:
+			return timeVal;
+		case TimeScaleType::Min:
+			return timeVal / 60.;
+		case TimeScaleType::Hour:
+			return timeVal / (60. * 60.);
+		case TimeScaleType::Day:
+			return timeVal / (60. * 60. * 24);
+		case TimeScaleType::Week:
+			return timeVal / (60. * 60. * 24 * 7);
+		case TimeScaleType::Month:
+			return timeVal / (60. * 60. * 24 * 7 * 30);
+		case TimeScaleType::Year:
+			return timeVal / (60. * 60. * 24 * 7 * 365);
+		}
+	}
+}
