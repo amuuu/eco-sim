daf625386fa0d866f8b20ea8f4b557996a5767f6
amu <aamouie01@gmail.com>
2023-02-09 18:04:29 +0330

    Convert some time units for timer

--- GameSystems/GeneralTools/Timer.h
+++ GameSystems/GeneralTools/Timer.h
@@ -5,6 +5,11 @@
 
 namespace General
 {
+	enum class TimeScaleType
+	{
+		Sec, Min, Hour, Day, Week, Month, Year
+	};
+
 	class BaseTimer
 	{
 	public:
@@ -48,6 +53,27 @@ namespace General
 			if (!hasStarted) return -1.f;
 
 			return Elapsed() * 1000.0f;
+		}
+
+		inline float GetElapsedInScale(TimeScaleType scale)
+		{
+			switch (scale)
+			{
+			case TimeScaleType::Sec:
+				return ElapsedMillis();
+			case TimeScaleType::Min:
+				return ElapsedMillis() / 60.;
+			case TimeScaleType::Hour:
+				return ElapsedMillis() / (60. * 60.);
+			case TimeScaleType::Day:
+				return ElapsedMillis() / (60. * 60. * 24);
+			case TimeScaleType::Week:
+				return ElapsedMillis() / (60. * 60. * 24 * 7);
+			case TimeScaleType::Month:
+				return ElapsedMillis() / (60. * 60. * 24 * 7 * 30);
+			case TimeScaleType::Year:
+				return ElapsedMillis() / (60. * 60. * 24 * 7 * 365);
+			}
 		}
 
 	private:
